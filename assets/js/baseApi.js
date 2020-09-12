91232406a99c47d4d4a7293f0371f14b769287cb
sxy <651908064@qq.com>
2020-09-12 09:58:03 +0800

    后台主页

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,16 +1,27 @@
+
+//ajaxPrefilter()是在调用Ajax时候提前调用此函数
 $.ajaxPrefilter(function (options) {
 
     options.url = 'http://ajax.frontend.itheima.net'+options.url
-    // console.log(options.url);
+    
+
     if (options.url.indexOf('/my') !== -1) {
         options.headers = {
             Authorization:localStorage.getItem('ID') || ''
         }
     }
    
+
+    //设置用户登录权限，必须要经过登录页面才可以登录
     options.complete =  function (res) {
+
+        //判断是否获取用户信息成功
         if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+
+            //清除本地缓存
             localStorage.removeItem('ID')
+
+            //强制退出登录界面
             location.href = '/login.html'
         }
      }
--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,7 @@ $(function() {
     // console.log(1);
     getData ()
 
-    $('.layui-nav-item').on('click',function() {
+    $('.exit').on('click',function() {
         console.log(1);
         layer.confirm('确定退出吗?', {icon: 3, title:'提示'}, function(index){
             localStorage.removeItem('ID')
@@ -20,6 +20,7 @@ function getData () {
             Authorization:localStorage.getItem('ID')
         },
         success:function (res) {
+           
             if (res.status == 0) {
                 swich_header(res.data)
             } else {
@@ -33,7 +34,7 @@ function getData () {
 //渲染头像
 function swich_header(data) {
     $('.welcome').html('欢迎&nbsp&nbsp'+data.username)
-    console.log(data);
+    
     if(data.user_pic !== null) {
         $('.layui-nav-img').attr('src',data.user_pic).show()
         return $('.head_img').hide()
